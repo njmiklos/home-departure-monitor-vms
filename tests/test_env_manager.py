69b2ae4f72072bc06a9a535dab10635b12057c73
Natalia Miklós <119075228+njmiklos@users.noreply.github.com>
2026-02-05 12:28:27 +0100

    Changed quotation marks

--- tests/test_env_manager.py
+++ tests/test_env_manager.py
@@ -11,37 +11,37 @@ def disable_dotenv_loading(monkeypatch):
     - during tests, load_dotenv() does nothing, only os.environ matters
     - monkeypatch.setenv() says what os.environ contains
     """
-    monkeypatch.setattr(env_manager, "load_dotenv", lambda: None)
+    monkeypatch.setattr(env_manager, 'load_dotenv', lambda: None)
 
 # OK
 
 def test_get_path_ok(monkeypatch):
     tmp_path = str('tmp_path')
-    monkeypatch.setenv("DATA_DIR", tmp_path)
-    result = env_manager.get_path("DATA_DIR")
+    monkeypatch.setenv('DATA_DIR', tmp_path)
+    result = env_manager.get_path('DATA_DIR')
     assert result == Path(tmp_path)
     assert isinstance(result, Path)
 
 def test_get_longitude_ok(monkeypatch):
-    monkeypatch.setenv("LONGITUDE", "12.917891093807997")
+    monkeypatch.setenv('LONGITUDE', '12.917891093807997')
     result = env_manager.get_longitude()
     assert result == 12.91789109
     assert isinstance(result, float)
 
 def test_get_latitude_ok(monkeypatch):
-    monkeypatch.setenv("LATITUDE", "50.84516430809533")
+    monkeypatch.setenv('LATITUDE', '50.84516430809533')
     result = env_manager.get_latitutde()
     assert result == 50.84516431
     assert isinstance(result, float)
 
 def test_get_radius_ok(monkeypatch):
-    monkeypatch.setenv("RADIUS", "500")
+    monkeypatch.setenv('RADIUS', '500')
     result = env_manager.get_radius()
     assert result == 500
     assert isinstance(result, int)
 
 def test_get_timeframe_ok(monkeypatch):
-    monkeypatch.setenv("TIMEFRAME", "30")
+    monkeypatch.setenv('TIMEFRAME', '30')
     result = env_manager.get_timeframe()
     assert result == 30
     assert isinstance(result, int)
@@ -49,26 +49,26 @@ def test_get_timeframe_ok(monkeypatch):
 # ValueErrors
 
 def test_get_path_error(monkeypatch):
-    monkeypatch.delenv("TMP_PATH", raising=False)
-    with pytest.raises(ValueError, match="TMP_PATH is not set in the .env file."):
-        env_manager.get_path("TMP_PATH")
+    monkeypatch.delenv('TMP_PATH', raising=False)
+    with pytest.raises(ValueError, match='TMP_PATH is not set in the .env file.'):
+        env_manager.get_path('TMP_PATH')
 
 def test_get_latitude_error(monkeypatch):
-    monkeypatch.delenv("LATITUDE", raising=False)
-    with pytest.raises(ValueError, match="LATITUDE is not set in the .env file."):
+    monkeypatch.delenv('LATITUDE', raising=False)
+    with pytest.raises(ValueError, match='LATITUDE is not set in the .env file.'):
         env_manager.get_latitutde()
 
 def test_get_longitude_error(monkeypatch):
-    monkeypatch.delenv("LONGITUDE", raising=False)
-    with pytest.raises(ValueError, match="LONGITUDE is not set in the .env file."):
+    monkeypatch.delenv('LONGITUDE', raising=False)
+    with pytest.raises(ValueError, match='LONGITUDE is not set in the .env file.'):
         env_manager.get_longitude()
 
 def test_get_radius_error(monkeypatch):
-    monkeypatch.delenv("RADIUS", raising=False)
-    with pytest.raises(ValueError, match="RADIUS is not set in the .env file."):
+    monkeypatch.delenv('RADIUS', raising=False)
+    with pytest.raises(ValueError, match='RADIUS is not set in the .env file.'):
         env_manager.get_radius()
 
 def test_get_timeframe_error(monkeypatch):
-    monkeypatch.delenv("TIMEFRAME", raising=False)
-    with pytest.raises(ValueError, match="TIMEFRAME is not set in the .env file."):
+    monkeypatch.delenv('TIMEFRAME', raising=False)
+    with pytest.raises(ValueError, match='TIMEFRAME is not set in the .env file.'):
         env_manager.get_timeframe()
